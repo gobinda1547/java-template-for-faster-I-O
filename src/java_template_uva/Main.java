cb07f006b573e7e52c2d9618e06ddc8ece75bf1c
Gobinda Paul <gobinda.piaul.1547@gmail.com>
2017-12-03 04:27:12 +0600

    removed all the errors, and big code, now it is okay

--- src/java_template_uva/Main.java
+++ src/java_template_uva/Main.java
@@ -31,18 +31,6 @@ public class Main {
 			}
 
 		}
-		
-		public static int getMaximumSubArraySum(int[] arr) {
-			if (arr.length <= 0)
-				return -1;
-
-			int currentSum = arr[0], globalSum = arr[0];
-			for (int i = 1; i < arr.length; i++) {
-				currentSum = Math.max(arr[i], currentSum + arr[i]);
-				globalSum = Math.max(globalSum, currentSum);
-			}
-			return globalSum;
-		}
 
 		public String readLine() {
 			try {
